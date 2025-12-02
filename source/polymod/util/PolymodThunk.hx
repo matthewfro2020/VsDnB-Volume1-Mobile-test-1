6de043ea3632ccc53c88250e34eecb4e33b4c9b7
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-02 16:38:58 -0500

    Delete source/polymod directory

--- source/polymod/util/PolymodThunk.hx
+++
@@ -1,7 +0,0 @@
-package polymod.util;
-
-// Removes thx.core dependency from polymod internals
-class PolymodThunk {
-    public static inline function tuple(a:Dynamic, b:Dynamic)
-        return { _1: a, _2: b };
-}
