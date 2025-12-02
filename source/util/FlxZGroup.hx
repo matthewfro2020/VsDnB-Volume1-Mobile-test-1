8fb6b0c44f0c7afb97807c95c7cf96240c15c884
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-02 11:41:44 -0500

    Create FlxZGroup.hx

---
+++ source/util/FlxZGroup.hx
@@ -0,0 +1,10 @@
+package util;
+
+import flixel.group.FlxSpriteGroup;
+import util.FlxZSprite;
+
+// A SpriteGroup that supports zIndex sorting
+class FlxZGroup extends FlxSpriteGroup
+{
+    public var zIndex:Int = 0;
+}
