36bafa67a60166abae911f879f72f8cd3733264d
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-02 10:18:18 -0500

    Update SpeakerRegistry.hx

--- source/data/dialogue/SpeakerRegistry.hx
+++ source/data/dialogue/SpeakerRegistry.hx
@@ -1,10 +1,8 @@
 package data.dialogue;
 
 import play.dialogue.ScriptedSpeaker;
-import openfl.utils.Assets;
 import json2object.JsonParser;
 import play.dialogue.Speaker;
-import play.dialogue.ScriptedDialogue;
 
 class SpeakerRegistry extends BaseRegistry<Speaker, SpeakerData>
 {
@@ -12,24 +10,27 @@ class SpeakerRegistry extends BaseRegistry<Speaker, SpeakerData>
     public static var VERSION_RULE:thx.semver.VersionRule = '1.0.x';
 
     public static var instance(get, never):SpeakerRegistry;
+    static var _instance:SpeakerRegistry;
 
     static function get_instance():SpeakerRegistry
     {
-        if (_instance == null) 
+        if (_instance == null)
             _instance = new SpeakerRegistry();
         return _instance;
     }
 
-    static var _instance:SpeakerRegistry;
-    
     public function new()
     {
-        super('SpeakerRegistry', 'speakers', VERSION_RULE);
+        super("SpeakerRegistry", "speakers", VERSION_RULE);
     }
+
+    //------------------------------------------------------------------
+    // PARSES THE JSON DATA FOR SPEAKER DEFINITIONS
+    //------------------------------------------------------------------
 
     public function parseEntryData(id:String):SpeakerData
     {
-        var parser:JsonParser<SpeakerData> = new JsonParser<SpeakerData>();
+        var parser = new JsonParser<SpeakerData>();
         parser.ignoreUnknownVariables = true;
 
         switch (loadEntryFile(id))
@@ -39,20 +40,39 @@ class SpeakerRegistry extends BaseRegistry<Speaker, SpeakerData>
             default:
                 return null;
         }
+
         if (parser.errors.length > 0)
-        {
             printErrors(parser.errors);
-        }
-        return parser.value; 
+
+        return parser.value;
     }
+
+    //------------------------------------------------------------------
+    // THIS IS THE IMPORTANT PART:
+    // ALWAYS RETURNS A SPEAKER OBJECT (FlxZSprite)
+    //------------------------------------------------------------------
+
+    public function fetchEntry(id:String):Speaker
+    {
+        var data = parseEntryData(id);
+        if (data == null)
+            return null;
+
+        // Creates a new Speaker instance based on the ID
+        return new Speaker(id);
+    }
+
+    //------------------------------------------------------------------
+    // SCRIPTED SPEAKERS (for dialogue scripts)
+    //------------------------------------------------------------------
 
     function createScriptedEntry(clsName:String):Speaker
     {
-        return ScriptedSpeaker.init(clsName, 'generic');
+        return ScriptedSpeaker.init(clsName, "generic");
     }
 
     function getScriptedClasses():Array<String>
     {
-       return ScriptedSpeaker.listScriptClasses();
+        return ScriptedSpeaker.listScriptClasses();
     }
 }
